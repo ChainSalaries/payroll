e294e8034a277a5a682f0b5eae1dffb434837ef2
Olivier de Swart <olivierdeswart@gmail.com>
2024-07-13 14:08:31 +0200

    successful blockscout verification

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,7 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
-const privateKeys = process.env.PRIVATE_KEYS || ""
+
+const privateKeys = process.env.PRIVATE_KEYS || "";
+const PRIVATE_KEY_BACKEND = process.env.PRIVATE_KEY_BACKEND;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -10,25 +12,39 @@ module.exports = {
     localhost: {},
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: privateKeys.split(",")
+      accounts: privateKeys.split(","),
     },
     bscTestnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
       chainId: 97,
       accounts: privateKeys.split(","),
-      gasPrice: 20000000000
+      gasPrice: 20000000000,
+    },
+    'base-sepolia': {
+      url: 'https://sepolia.base.org',
+      accounts: [PRIVATE_KEY_BACKEND],
+      gasPrice: 1000000000,
     },
     mainnet: {
       url: "https://bsc-dataseed.bnbchain.org/",
       chainId: 56,
       gasPrice: 20000000000,
-      accounts: privateKeys.split(",")
-    }
+      accounts: privateKeys.split(","),
+    },
   },
   etherscan: {
     apiKey: {
-      bsc: process.env.BSCSCAN_API_KEY,
-      bscTestnet: process.env.BSCSCAN_API_KEY
-    }
-  }
+      "base-sepolia": process.env.BLOCKSCOUT_API_KEY,
+    },
+    customChains: [
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://base-sepolia.blockscout.com/api",
+          browserURL: "https://base-sepolia.blockscout.com",
+        },
+      },
+    ],
+  },
 };
