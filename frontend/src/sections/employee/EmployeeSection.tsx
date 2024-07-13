c08cfb70614efefc3472f4a26717fb5178e85fb9
Joonas Köppä <joonas.koppa@smartcontract.com>
2024-07-13 16:19:30 +0200

    separate world id component

--- frontend/src/sections/employee/EmployeeSection.tsx
+++ frontend/src/sections/employee/EmployeeSection.tsx
@@ -6,31 +6,13 @@ import Box from '@mui/material/Box'
 import { Button, Stack, TextField, Typography } from '@mui/material'
 import { IDKitWidget, ISuccessResult, VerificationLevel } from '@worldcoin/idkit'
 import { useAccount } from 'wagmi'
+import WorldID from './WorldID'
 //import fetch from 'node-fetch'
 
 export default function VerifyWorldID() {
   const dispatch = useAppDispatch()
   const { address, isConnecting, isDisconnected } = useAccount()
   console.log("address: " + address)
-  const onSuccess = (result: ISuccessResult) => {
-    console.log("success")
-  }
-  const verifyProof = async (result: ISuccessResult) => {
-    //const proof = JSON.stringify(result) // worldID proof
-    //console.log(result)
-    const response = await fetch("/api/verify", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        proof: result,
-        verification_level: "device",
-        organization: 5,
-        signal: address
-      })
-    })
-  }
 
   return (
     <div>
@@ -49,16 +31,9 @@ export default function VerifyWorldID() {
             borderRadius: 1,
           }}
         >
-          <IDKitWidget
-            app_id="app_staging_d159dd1d864c0bd25f6341f2f4a9cbc5" // obtained from the Developer Portal
-            action="employee-verification" // this is your action name from the Developer Portal
-            signal={address} // any arbitrary value the user is committing to, e.g. a vote
-            onSuccess={onSuccess}
-            handleVerify={verifyProof}
-            verification_level={VerificationLevel.Device} // minimum verification level accepted, defaults to "orb"
-          >
-            {({ open }) => <button onClick={open}>Verify with World ID</button>}
-          </IDKitWidget>
+          <WorldID
+            address={address}
+          />
         </Box>
       </Stack>
     </div>
@@ -66,5 +41,33 @@ export default function VerifyWorldID() {
 
 
 }
+
+
+// import { type BaseError, useReadContract } from 'wagmi'
+
+// function ReadContract() {
+//   const { 
+//     data: balance,
+//     error,
+//     isPending
+//   } = useReadContract({
+//     ...wagmiContractConfig,
+//     functionName: 'balanceOf',
+//     args: ['0x03A71968491d55603FFe1b11A9e23eF013f75bCF'],
+//   })
+
+//   if (isPending) return <div>Loading...</div>
+
+//   if (error)
+//     return (
+//       <div>
+//         Error: {(error as BaseError).shortMessage || error.message}
+//       </div>
+//     )
+
+//   return (
+//     <div>Balance: {balance?.toString()}</div>
+//   )
+// }
 
 
---
+++ frontend/src/sections/employee/WorldID.tsx
@@ -0,0 +1,41 @@
+import { IDKitWidget, ISuccessResult, VerificationLevel } from '@worldcoin/idkit'
+
+type Props = {
+    address: `0x${string}` | undefined
+}
+export default function VerifyWorldID({ address }: Props) {
+    const onSuccess = (result: ISuccessResult) => {
+        console.log("success")
+    }
+    const verifyProof = async (result: ISuccessResult) => {
+        //const proof = JSON.stringify(result) // worldID proof
+        //console.log(result)
+        const response = await fetch("/api/verify", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                proof: result,
+                verification_level: "device",
+                organization: 5,
+                signal: address
+            })
+        })
+    }
+
+    return (
+        <div>
+            <IDKitWidget
+                app_id="app_staging_d159dd1d864c0bd25f6341f2f4a9cbc5" // obtained from the Developer Portal
+                action="employee-verification" // this is your action name from the Developer Portal
+                signal={address} // any arbitrary value the user is committing to, e.g. a vote
+                onSuccess={onSuccess}
+                handleVerify={verifyProof}
+                verification_level={VerificationLevel.Device} // minimum verification level accepted, defaults to "orb"
+            >
+                {({ open }) => <button onClick={open}>Verify with World ID</button>}
+            </IDKitWidget>
+        </div>
+    )
+}
